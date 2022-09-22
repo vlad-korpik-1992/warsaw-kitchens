13355ec7daff21299c6d8374be101e758f47341c
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-09-22 16:56:23 +0300

    finished cart page desktop version

--- js/main.js
+++ js/main.js
@@ -13,4 +13,31 @@ $(document).ready(function() {
 		$('.fittings__content').removeClass('fittings__content--active');
 		jQuery("#"+id).addClass('fittings__content--active');
 	});
+	$('.cart__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.cart__sliderprev'
+	});
+	$('.cart__sliderprev').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		infinite: true,
+		asNavFor: '.cart__slider',
+		dots: false,
+		arrows: false,
+		focusOnSelect: true,
+		responsive: [
+		  {
+			breakpoint: 767,
+			settings: {
+				dots: false,
+				slidesToShow: 2,
+				rows: 1,
+				centerMode: false
+			}
+		  }
+		]
+	});
 });
